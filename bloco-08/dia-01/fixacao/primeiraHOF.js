f50012248b46fc5b111f0d2e68b644af10c4b8ea
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-03-29 16:02:14 -0300

    Adicionando primeira HOF

---
+++ bloco-08/dia-01/fixacao/primeiraHOF.js
@@ -0,0 +1,9 @@
+const acordar = () => console.log('Acordando!!');
+
+const comer = () => console.log('Bora tomar café!!');
+
+const dormir = () => console.log('Partiu dormir!!');
+
+const doingThings = (funct) => funct();
+
+doingThings(acordar);

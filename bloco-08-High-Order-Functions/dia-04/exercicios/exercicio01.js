c7b57408ab16a1dd250e76a658f4b05d9b95fac8
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-04-19 19:12:34 -0300

    Adicionando commits bloco 8

---
+++ bloco-08-High-Order-Functions/dia-04/exercicios/exercicio01.js
@@ -0,0 +1,23 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+function flatten() {
+  // Dada uma matriz de matrizes, transforme em uma única matriz.
+  // escreva seu código aqui.
+  const newArray = [];
+  arrays.reduce((cont, actual) => {}, []);
+//   newArray = arrays
+//     .forEach((element) => element)
+//     .reduce((acc, curr) => acc + newArray.push(curr),[]);
+   return newArray;
+}
+
+console.log(flatten());
+
+//assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);

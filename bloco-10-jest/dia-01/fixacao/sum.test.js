982e6ce8f776b7c3556fe408a4c17d0696032153
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-04-19 16:22:10 -0300

    Exercicio de fixacao jest

---
+++ bloco-10-jest/dia-01/fixacao/sum.test.js
@@ -0,0 +1,11 @@
+test('dois mais dois', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe e toEqual são equivalentes para números
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
